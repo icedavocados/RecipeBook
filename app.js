330bad1f6ec48343570ff7dfab3d375cd2be169c
icedavocados <77272746+icedavocados@users.noreply.github.com>
2022-06-08 23:06:36 +0700

    Update app.js

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 const validateRecipe = (req, res, next) => {
     const { error } = recipeSchema.validate(req.body);
     if (error) {

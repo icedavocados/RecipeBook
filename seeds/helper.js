d918b241c97de24618566e7952fcb1961a812dab
icedavocados <77272746+icedavocados@users.noreply.github.com>
2022-06-09 23:29:17 +0700

    Create helper.js

---
+++ seeds/helper.js
@@ -0,0 +1,29 @@
+module.exports.meats = [
+    'Chicken',
+    'Beef',
+    'Lamb',
+    'Fish',
+    'Duck',
+    'Lobster',
+    'Oyster',
+    'Pork',
+    'Scallop',
+    'Turkey',
+    'Crab',
+    'Venison',
+    'Clam',
+    'Shrimp',
+    'Prawn',
+    'Rabbit',
+    'Squid'
+]
+
+module.exports.cooks = [
+    'Grilled',
+    'Braised',
+    'Roasted',
+    'Stir Fried',
+    'Pan Fried',
+    'Boiled',
+    'Sautéd'
+]

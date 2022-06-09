702e14566dc2fd13809262de8348fd0cff3313c7
icedavocados <77272746+icedavocados@users.noreply.github.com>
2022-06-09 23:30:25 +0700

    Update schemas.js

--- schemas.js
+++ schemas.js
@@ -2,6 +2,7 @@ const Joi = require('joi');
 module.exports.recipeSchema = Joi.object({
     recipe: Joi.object({
         title: Joi.string().required(),
+        author: Joi.string().required(),
         description: Joi.string().required()
     }).required()
 })

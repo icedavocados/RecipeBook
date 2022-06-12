300a1fb378f7ae365e199e5f4ec6a18ae33e07b1
icedavocados <77272746+icedavocados@users.noreply.github.com>
2022-06-13 01:47:07 +0700

    Update schemas.js

--- schemas.js
+++ schemas.js
@@ -2,7 +2,23 @@ const Joi = require('joi');
 module.exports.recipeSchema = Joi.object({
     recipe: Joi.object({
         title: Joi.string().required(),
+        image: Joi.string().required(),
         author: Joi.string().required(),
         description: Joi.string().required()
+    }).required()
+})
+
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number(),
+        body: Joi.string().required()
+    }).required()
+})
+
+module.exports.userSchema = Joi.object({
+    user: Joi.object({
+        username: Joi.string().required(),
+        email: Joi.string().required(),
+        password: Joi.string().required()
     }).required()
 })

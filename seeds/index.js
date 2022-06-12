cb62e419fe0eaa55c22c02d2c40e88a4d92e0863
icedavocados <77272746+icedavocados@users.noreply.github.com>
2022-06-13 01:38:25 +0700

    Update index.js

--- seeds/index.js
+++ seeds/index.js
@@ -16,8 +16,10 @@ const seedDB = async() => {
     for (let i = 0; i < 50; i++) {
         const newRecipe = new Recipe({
             title: `${randomArray(cooks)} ${randomArray(meats)}`,
+            image: 'https://source.unsplash.com/collection/1424340',
             author: 'Super Admin',
-            description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat, quo nam, alias excepturi perferendis ipsa ex sapiente magni consectetur blanditiis itaque officiis voluptate vero esse dolor, animi mollitia architecto earum?'
+            description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat, quo nam, alias excepturi perferendis ipsa ex sapiente magni consectetur blanditiis itaque officiis voluptate vero esse dolor, animi mollitia architecto earum?',
+            steps: '1. First\n2. Second\n 3. Third'
         })
         await newRecipe.save();
     }

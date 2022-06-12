a178b382a4bc0da0ba7f64a6166b13174998a37c
icedavocados <77272746+icedavocados@users.noreply.github.com>
2022-06-13 01:33:53 +0700

    Update recipe.js

--- modules/recipe.js
+++ modules/recipe.js
@@ -1,10 +1,28 @@
 const mongoose = require('mongoose');
+const Comment = require('./comment');
+const User = require('./user');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
     title: String,
+    image: String,
     author: String,
-    description: String
+    description: String,
+    steps: String,
+    comments: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Comment'
+    }]
+});
+
+recipeSchema.post('findOneandDelete', async function(doc) {
+    if (doc) {
+        await Comment.deleteMany({
+            _id: {
+                $in: doc.comments
+            }
+        })
+    }
 })
 
 module.exports = mongoose.model('Recipe', recipeSchema);
